6248c970d9787b488d0c4289d1e85e9e57b39a54
itachi-coderr <umar124547@gmail.com>
2025-08-29 15:25:59 +0500

    register user

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express, { urlencoded } from "express"
+import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
@@ -13,5 +13,11 @@ app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//// routes importing
+
+import { router } from "./routes/user.routes.js"
+
+app.use("/api/v1/users", router)
 
 export { app }
